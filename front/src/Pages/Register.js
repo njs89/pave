7e43de7437d9a458f39e48c221f46c94c786cd46
Jona <44445775+njs89@users.noreply.github.com>
2022-02-07 17:21:14 +0100

    Add files via upload

--- front/src/Pages/Register.js
+++ front/src/Pages/Register.js
@@ -3,10 +3,20 @@ import { Form, Button, Card } from "react-bootstrap"
 import { Link } from "react-router-dom";
 
 export default function Register() {
-   const emailRef = useRef()
-   const passwordRef = useRef()
+   const username = useRef()
+   const password = useRef()
    const passwordConfirmRef = useRef()
-   
+   const signup = (event) => {
+      event.preventDefault()
+      const params = { username, password}
+      const options = {method: 'POST', body: JSON.stringify(params)}
+
+      fetch('api/register', options).then( response => response.json()).then(data => {
+         const {token} = data
+      })
+      //create request and send it to server//
+      }
+
    return (
       <>
          <Card>
@@ -15,17 +25,17 @@ export default function Register() {
                <Form>
                   <Form.Group id="email">
                      <Form.Label>Email</Form.Label>
-                     <Form.Control type="email" ref={emailRef} required />
+                     <Form.Control type="email" ref={username} required />
                   </Form.Group>
                   <Form.Group id="password">
                      <Form.Label>Password</Form.Label>
-                     <Form.Control type="password" ref={passwordRef} required />
+                     <Form.Control type="password" ref={password} required />
                   </Form.Group>
                   <Form.Group id="password-confirm">
                      <Form.Label>Password confirmation</Form.Label>
                      <Form.Control type="password" ref={passwordConfirmRef} required />
                   </Form.Group>
-                  <Button className="w-100" type="submit">Sign Up</Button>
+                  <Button className="w-100" type="submit" onClick={signup} >Sign Up</Button>
                </Form>
             </Card.Body>
          </Card>
